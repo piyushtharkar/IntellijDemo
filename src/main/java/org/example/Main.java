81db88bcae421e8746695dda948667bf8b1bf6bd
piyushtharkar <piyushtharkar5@gmail.com>
2025-01-31 13:17:04 +0530

    a line added in main to print

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("its a demo project for git integration in intellij");
     }
 }
